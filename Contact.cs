d77a0bf00f653b303949f7df69d01c6ab3aff401
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-27 20:25:49 +0530

    UC2

---
+++ Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+
+    public class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+        public double phoneNo { get; set; }
+        public string emailId { get; set; }
+
+    }
+}
