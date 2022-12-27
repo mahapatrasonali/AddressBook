9402da8ee74d34f4571ae7ca2579f2f3c4adaa81
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-27 20:50:27 +0530

    UC3

--- CreateContact.cs
+++ CreateContact.cs
@@ -9,16 +9,114 @@ namespace AddressBookSystem
 {
     public class CreateContact
     {
-       
-    public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public long Zip { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Email { get; set; }
-
+        Contact contact = new Contact();
+        List<Contact> people = new List<Contact>();
       
+        public void AddPerson()     
+        {
+            contact = new Contact();
+            Console.WriteLine("Enter your First Name");
+            contact.firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
+            contact.lastName = Console.ReadLine();
+            Console.WriteLine("Enter your Address");
+            contact.address = Console.ReadLine();
+            Console.WriteLine("Enter your Phone Number");
+            contact.phoneNo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your email id");
+            contact.emailId = Console.ReadLine();
+            people.Add(contact);
+        }
+        public void EditPerson()    //Editing Contact .
+        {
+        findingPersonAgain:
+            Console.WriteLine("Enter the first name of person to be removed");
+            string verifyingFirstName = Console.ReadLine();
+            Console.WriteLine("Enter the last name of person to be removed");
+            string verifyingLastName = Console.ReadLine();
+            foreach (var person in people)
+            {
+                if (person.firstName.Equals(verifyingFirstName) && person.lastName.Equals(verifyingLastName))   //Editing Contact if exists.
+                {
+                moreEditing:
+                    Console.WriteLine("Please select any one as per the below given options on which you want the modification \n" +
+                        "1)First Name\n2)Last Name\n3)Address\n4)Phone Number\n5)Email_Id");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Please enter your first name : ");
+                            person.firstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Please enter your last name : ");
+                            person.lastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Please enter your Address : ");
+                            person.address = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Please enter your Phone Number : ");
+                            person.phoneNo = Convert.ToDouble(Console.ReadLine());
+                            break;
+                        case 5:
+                            Console.WriteLine("Please enter your email Id: ");
+                            person.emailId = Console.ReadLine();
+                            break;
+                        default:
+                            Console.WriteLine("You've selected an Invalid Option!");
+                            Console.WriteLine("Do you want to try again ");
+                            if (Console.ReadKey().Key == ConsoleKey.Y)
+                            {
+                                goto findingPersonAgain;
+                            }
+                            break;
+                    }
+                    Console.WriteLine("Do you want to do any more modification in the same contact Y/N \n");
+                    if (Console.ReadKey().Key == ConsoleKey.Y)
+                    {
+                        goto moreEditing;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("That person could not be found.Do you want to try again ");
+                    if (Console.ReadKey().Key == ConsoleKey.Y)
+                    {
+                        goto findingPersonAgain;
+                    }
+                    return;
+                }
+            }
+        }
+        public void ListPeople()        //List of all Contacts/People's.
+        {
+            if (people.Count == 0)
+            {
+                Console.WriteLine("Your Address book is empty. Press any key to continue");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people found in your address book :");
+            foreach (var person in people)
+            {
+                Console.WriteLine($"\n First Name : {contact.firstName}\n Last Name : {contact.lastName}\n Address : {contact.address}\n Phone Number : {contact.phoneNo}\n Email ID : {contact.emailId}");
+            }
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
     }
 }
+
+    
+
+
+
+  
+
+
