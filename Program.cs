370aedea5694d19dee5f77bd888e5f6aa84ee3cc
[mahapatrasonali] <[sonangr2000@gmail.com]>
2022-12-27 14:24:16 +0530

    Create Address Book

--- Program.cs
+++ Program.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book Program");
-            Console.ReadKey();
+              Console.WriteLine("Welcome to the Address Book System Problem.");
+            CreateContact contact = new CreateContact()
+            {
+
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNumber = Console.ReadLine(),
+                Email = Console.ReadLine(),
+                
+             };
+            
+           Console.WriteLine("Contact Details : " + "\n" + "First Name : " + contact.FirstName + "\n" + "Last Name : " + contact.LastName  + "\n" + "Address is : " + contact.Address + "\n" + "City is : " + contact.City + "\n" + "State is : " + contact.State  + "\n" + "Zip is : " + contact.Zip  + "\n" + "Phone Number is :" + contact.PhoneNumber + "\n" + "Email is : " + contact.Email);
         }
     }
 }
+        
+         
+      
+
+
